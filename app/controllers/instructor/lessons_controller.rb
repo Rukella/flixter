1ca55f4b60392f49a85bf8dc7091f979c80a8461
Rukevwe <rukevwe2006@gmail.com>
2019-08-10 21:02:59 +0000

    course detail & lesson page

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -2,14 +2,8 @@ class Instructor::LessonsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_section
 
-
-  def new
-    if current_section.course.user != current_user
-      return render plain: 'Unauthorized', status: :Unauthorized
-    end
-
     def new
-    @lesson = Lesson.new
+      @lesson = Lesson.new
     end
 
    def create
@@ -28,6 +22,7 @@ class Instructor::LessonsController < ApplicationController
   helper_method :current_section
   def current_section
     @current_section ||= Section.find(params[:section_id])
+  end
 
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle)
